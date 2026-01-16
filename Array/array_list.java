645291fc2951c958566336369b85ccc47d7164e3
Suraj Pal <surajpal7442@gmail.com>
2026-01-16 23:06:58 +0530

    array_list

---
+++ Array/array_list.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class array_list {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+
+        // elements
+        list.add(0);
+        list.add(2);
+        list.add(4);
+        System.out.println(list);
+
+        // get elem
+        int elem = list.get(1);
+        System.out.println(elem);
+    }
+}
