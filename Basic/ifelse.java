53f4f3c36fcc476055463f9de4faf1930fb09133
Suraj Pal <surajpal7442@gmail.com>
2026-01-25 15:02:42 +0530

    If Else

---
+++ Basic/ifelse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter Age: ");
+            int age = sc.nextInt();
+
+            if(age >= 18){
+                System.out.println("Adult");
+            } else{
+                System.out.println("Not Adult");
+            }
+        }
+    }
+}
