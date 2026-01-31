42d94e2bc858e62d9eb2faab871c8de839135fb7
Suraj Pal <surajpal7442@gmail.com>
2026-01-31 22:46:30 +0530

    HollowRhombos pattern

---
+++ Pattern/hollowRhombos.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class hollowRhombos {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter value: ");
+            int n = sc.nextInt();
+
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=n;k++){
+                    if(k==1||k==n||i==n||i==1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
