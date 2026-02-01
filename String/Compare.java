541c40cdae21252d459f118d8e0c99be71581db1
Suraj Pal <surajpal7442@gmail.com>
2026-02-01 21:37:26 +0530

    CompareString and SubString program

---
+++ String/Compare.java
@@ -0,0 +1,14 @@
+public class Compare {
+    
+    public static void main(String[] args) {
+        
+        String name1 = "Suraj";
+        String name2 = "Suraj2";
+
+        if(name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not rqual");
+        }
+    }
+}
---
+++ String/SubString.java
@@ -0,0 +1,9 @@
+public class SubString {
+    public static void main(String[] args) {
+         
+        String sentence = "My name is Suraj";
+        String name = sentence.substring(1, 6);
+
+        System.out.println("SubString is:" + name);
+    }
+}
